e359ab6dbf43aacd198cc54f4553f159cf2273a7
Sergey Vorontsov <sevor005@yandex.ru>
2025-01-21 14:32:24 +0700

    refactor: eslint 01-read-file.

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
+const readStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf8',
+);
 
 readStream.pipe(process.stdout);
 
 readStream.on('error', (error) => {
-    console.log(error);
+  console.log(error);
 });
